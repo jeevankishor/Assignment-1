e3ff3cadf05d53d230ca72f8ccd95c06876f70f9
jeevankishor <jeevan.kishore@raremile.com>
2015-06-24 16:28:39 +0530

    GitApp.java file modidified as per requirement

--- src/com/raremile/training/jeevankishore/GitApp.java
+++ src/com/raremile/training/jeevankishore/GitApp.java
@@ -3,7 +3,7 @@ package com.raremile.training.jeevankishore;
 public class GitApp {
 
 	public static void main(String[] args) {
-		System.out.println("My name is JK. File modified for Part 2.");
+		System.out.println("My name is JK, I know some Git");
 
 	}
 
