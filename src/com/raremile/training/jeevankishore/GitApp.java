68fa9577ab6b299e5fd36559705c91778cb6ad6a
Sriram Keerthi M K <sriram@raremile.com>
2015-06-24 15:53:17 +0530

    Update GitApp.java

--- src/com/raremile/training/jeevankishore/GitApp.java
+++ src/com/raremile/training/jeevankishore/GitApp.java
@@ -3,7 +3,7 @@ package com.raremile.training.jeevankishore;
 public class GitApp {
 
 	public static void main(String[] args) {
-		System.out.println("My name is JK");
+		System.out.println("My name is JK. File modified for Part 2.");
 
 	}
 
