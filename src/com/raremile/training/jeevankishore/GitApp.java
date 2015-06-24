4111fc867a08b273194891962c42d350da85cd6e
jeevankishor <jeevan.kishore@raremile.com>
2015-06-24 17:11:09 +0530

    Fixing Conflicts

--- src/com/raremile/training/jeevankishore/GitApp.java
+++ src/com/raremile/training/jeevankishore/GitApp.java
@@ -3,7 +3,7 @@ package com.raremile.training.jeevankishore;
 public class GitApp {
 
 	public static void main(String[] args) {
-		System.out.println("My name is JK, I'm learning Git");
+		System.out.println("My name is JK, I know a lot more Git. I know how to merge!");
 
 	}
 
